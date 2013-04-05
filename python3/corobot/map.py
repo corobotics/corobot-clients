331f19a1568ab6e14bcd0bef6cbf3c342a2db6d0
Max Bogue <max@maxbogue.com>
2013-04-05 12:10:45 -0400

    Cleanup/rewrite map.py.
    Might need to change more things if we decide to get data from the API
    instead of the CSV.

--- python3/corobot/map.py
+++ python3/corobot/map.py
@@ -1,78 +1,47 @@
+"""This module represents the map for use in navigation.
+
+Authors: Z. Butler, M. Bogue
+
 """
-This module represents the map for use in navigation.
+from collections import namedtuple
 
-MapNode class will be used and returned by several functions.
+class Map():
 
-Author: Z. Butler
-"""
-class MapNode():
-    __slots__ = ('x','y','name','nbrs')
+    Node = namedtuple("Node", "name, x, y, neighbors")
 
-    def __init__(self,name,x,y):
-        self.x = x
-        self.y = y
-        self.name = name
-        self.nbrs = []
+    def __init__(self, mapFile):
+        self.nodes = {}
+        with open(mapFile) as f:
+            f.next() # skip the first line
+            for line in f:
+                parts = line.split(",")
+                name = parts[0]
+                x, y = float(parts[3]), float(parts[4])
+                neighbors = parts[6:]
+                self.nodes[name] = Node(name, x, y, neighbors)
 
-def _make_map():
-    """
-    Internal function, creates the map object from the file
-    """
-    nodes = {}
-    first = True
-    for line in open('../../corobot_map/map/waypoints.csv'):
-        if first:
-            first = False
-            continue
-        parts = line.split(',')
-        node = MapNode(parts[0].upper(),float(parts[3]),float(parts[4]))
-        node.nbrs = parts[6:]
-        nodes[node.name] = node
-    return nodes
-    
+    def get_closest_node(self, pos):
+        """Return the name of the closest node to the given position."""
+        min_dist = float('+infinity')
+        closest = None
+        for name, x, y, _ in self.nodes.values():
+            dx = pos.x - x
+            dy = pos.y - y
+            # Just use square distance since we're only comparing them.
+            dist = dx * dx + dy * dy
+            if dist < min_dist:
+                min_dist = dist
+                closest = name
+        return closest
 
-def get_closest_node(pos):
-    """
-    Returns the closest node to the given position
-    
-    pos - (x,y) location desired
+    def get_node_names(self):
+        """Returns names of all nodes in the map."""
+        return self.nodes.keys()
 
-    returns the name of the closest node
-    """
-    global __theMap__
-    minsqdist = float('+infinity')
-    closest = None
-    for nodename in __theMap__:
-        node = __theMap__[nodename]
-        sqdist = (node.x-pos[0])*(node.x-pos[0]) + (node.y-pos[1])*(node.y-pos[1])
-        if sqdist < minsqdist:
-            minsqdist = sqdist
-            closest = node.name
-    return closest
+    def get_node(self.name):
+        """Returns a Map.Node object for the given name."""
+        return self.nodes[name.upper()]
 
-def get_node_names():
-    """
-    Returns names of all nodes in the map
-    """
-    global __theMap__
-    return __theMap__.keys()
-
-def get_node(name):
-    """
-    Returns a MapNode object for the given name
-    """
-    global __theMap__
-    return __theMap__[name.upper()]
-
-def is_node(name):
-    """
-    Returns true/false whether the given name represents a node in the map
-    
-    name - node name to test
-    """
-    global __theMap__
-    return name in __theMap__
-
-
-__theMap__ = _make_map()
-
+    def is_node(name):
+        """Returns whether the given name represents a node in the map."""
+        return name.upper() in self.nodes
