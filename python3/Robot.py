a0bf8abcb61d3c6033b1a817f2225cbde6434492
Max Bogue <max@maxbogue.com>
2013-04-05 09:58:39 -0400

    Triple double quotes, not triple single quotes.

--- python3/Robot.py
+++ python3/Robot.py
@@ -1,10 +1,10 @@
-'''
+"""
 Python 3 user library for corobots project
 
 Based on Java library
 
 Z. Butler, Jan 2013
-'''
+"""
 import socket
 import os
 import RobotMap
@@ -16,17 +16,17 @@ class Robot():
     __slots__ = ('sockin', 'sockout', 'USER_PORT')
 
     def __init__(self):
-        '''
+        """
            Creates connection to robot
-        '''
+        """
         print('Connecting to robot...')
         self.USER_PORT = 15001
         self._open_socket()
 
     def _open_socket(self):
-        '''
+        """
         Creates the connection to the robot.
-        '''
+        """
         try:
             robotaddr = os.getenv('ROBOT')
             sock = socket.create_connection((robotaddr,self.USER_PORT))
@@ -37,14 +37,14 @@ class Robot():
             raise RobotConnectionException()
     
     def navigate_to_location(self,location,block):
-        '''
+        """
         Drives the robot to the given location, including a full path plan.
 
         location - destination: name of a map node (waypoint)
         block - whether or not to block (wait for destination to be reached before returning)
 
         Returns whether or not location was reached successfully
-        '''
+        """
         location = location.upper()
         if RobotMap.is_node(location):
             msg = 'NAVTOLOC ' + location + '\n'
@@ -58,14 +58,14 @@ class Robot():
             return False
 
     def go_to_location(self,location,block):
-        '''
+        """
         Drives the robot in a straight line to the given location.
 
         location - destination: name of a map node (waypoint)
         block - whether or not to block (wait for destination to be reached before returning)
 
         Returns whether or not location was reached successfully
-        '''
+        """
         location = location.upper()
         if RobotMap.is_node(location):
             msg = 'GOTOLOC ' + location + '\n'
@@ -79,13 +79,13 @@ class Robot():
             return False
 
     def go_to_XY(self,x,y,block):
-        '''
+        """
         Drives the robot in a straight line to the given coordinates.
         x, y - location to drive to
         block - whether or not to block (wait for destination to be reached before returning)
 
         Returns whether or not location was reached successfully
-        '''
+        """
         msg = 'GOTOXY ' + str(x) + ' ' + str(y) + '\n'
         self.sockout.write(msg)
         self.sockout.flush()
@@ -95,18 +95,18 @@ class Robot():
             return True
 
     def _query_arrive(self):
-        '''
+        """
         Internal method used for blocking gotos
-        '''
+        """
         self.sockout.write('QUERY_ARRIVE\n')
         self.sockout.flush()
         gotback = self.sockin.readline()
         return gotback[:6] == 'ARRIVE'
 
     def get_pos(self):
-        ''' 
+        """ 
         Asks robot for its position, returns an (x,y) tuple
-        '''
+        """
         self.sockout.write('GETPOS\n')
         self.sockout.flush()
         posstr = self.sockin.readline()
@@ -117,8 +117,8 @@ class Robot():
             return (float(posparts[1]),float(posparts[2]))
 
     def get_closest_loc(self):
-        '''
+        """
         Returns the closest node to the current robot location
-        '''
+        """
         pos = self.get_pos()
         return RobotMap.get_closest_node(pos)
