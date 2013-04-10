94df69cf7ff63f35c1998eeab4eab42aa96451ba
Max Bogue <max@maxbogue.com>
2013-04-10 14:05:59 -0400

    Update example.py

--- python3/example.py
+++ python3/example.py
@@ -1,23 +1,15 @@
-'''
-Simple example showing usage of the Python version of the corobot user API.
+"""Simple example showing usage of the Corobot user API."""
 
-author: Z. Butler
-'''
-from Robot import *
+from corobot import Robot
+
+def callback():
+    print("arrived!")
 
 def main():
-    '''
-      Sample program, drives the robot around a bit.
-    '''
-    r = Robot()
-    pos = r.get_pos()
-    print('Robot is at',pos)
-    loc = r.get_closest_loc()
-    print('Closest node is',loc)
-    #r.go_to_location(loc,True)
-    if r.go_to_XY(pos[0]-1.5, pos[1]+1.0, True):
-        print('Made it!')
-    else:
-        print("Didn't make it.")
+    """Test out simple API stuff."""
+    with Robot("127.0.1.1", 15001) as r:
+        p = r.go_to_xy(5, 5).then(callback)
+        p.wait()
+        print("fin")
 
 main()
