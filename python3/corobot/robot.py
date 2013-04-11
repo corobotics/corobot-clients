f9d7a04e6cf903cdb42a63da7def3e182a2b98d2
Max Bogue <max@maxbogue.com>
2013-04-11 12:51:23 -0400

    Rewrite all socket I/O to use asyncore/asynchat.

--- python3/corobot/robot.py
+++ python3/corobot/robot.py
@@ -4,6 +4,7 @@ Z. Butler, Jan 2013
 M. Bogue, April 2013
 
 """
+import asyncore
 from collections import deque
 import os
 from queue import Queue
@@ -12,62 +13,50 @@ from threading import Event, Lock, Thread
 
 from corobot.common import CorobotException
 from corobot.future import Future
+from corobot.io import CorobotClient
 from corobot.map import Map
 
 class Robot():
 
-    def __init__(self, addr, port):
+    def __init__(self, host, port):
         """Creates connection to robot."""
-        try:
-            self.socket = socket.create_connection((addr, port))
-        except OSError:
-            raise CorobotException("Couldn't connect to robot at %s:%d" % (addr, port))
         self.next_msg_id = 1
-        self.msg_queue = deque()
-        self.msg_queue_event = Event()
         self.futures = {}
-        self.socket_out = self.socket.makefile("w")
         self.out_lock = Lock()
-        self.reader_thread = Thread(target=self._socket_reader)
-        self.message_thread = Thread(target=self._message_handler)
-        self.running = True
-        self.reader_thread.start()
-        self.message_thread.start()
+        self.connected_event = Event()
+        self.error_connecting = False
+        self.client = CorobotClient(host, port, self)
+        self.io_thread = Thread(target=self._io_loop)
+        self.io_thread.start()
+        self.connected_event.wait()
+        if self.error_connecting:
+            raise CorobotException("Couldn't connect to robot at %s:%d" % (host, port))
 
-    def _socket_reader(self):
-        with self.socket.makefile("r") as socket_in:
-            for line in socket_in:
-                self.msg_queue.append(line)
-                self.msg_queue_event.set()
+    def _io_loop(self):
+        asyncore.loop(0.1)
 
-    def _message_handler(self):
-        while self.running:
-            while self.msg_queue:
-                msg = self.msg_queue.popleft()
-                tokens = msg.split(" ")
-                msg_id = int(tokens[0])
-                key = tokens[1]
-                data = tokens[2:]
-                future = self.futures.pop(msg_id)
-                if key == "POS":
-                    future._data = tuple(map(float, data))
-                if key != "ERROR":
-                    for callback in future._callbacks:
-                        callback()
-                else:
-                    future._error = " ".join(data)
-                    for error_callback in future._error_callbacks:
-                        error_callback(future._error)
-                future._event.set()
-            self.msg_queue_event.wait()
-            self.msg_queue_event.clear()
+    def _robot_response(self, msg):
+        tokens = msg.split(" ")
+        msg_id = int(tokens[0])
+        key = tokens[1]
+        data = tokens[2:]
+        future = self.futures.pop(msg_id)
+        if key == "POS":
+            future._data = tuple(map(float, data))
+        if key != "ERROR":
+            for callback in future._callbacks:
+                callback()
+        else:
+            future._error = " ".join(data)
+            for error_callback in future._error_callbacks:
+                error_callback(future._error)
+        future._event.set()
 
     def _write_message(self, msg):
         with self.out_lock:
             msg_id = self.next_msg_id
             self.next_msg_id += 1
-            self.socket_out.write("%d %s\n" % (msg_id, msg))
-            self.socket_out.flush()
+            self.client.write_line("%d %s" % (msg_id, msg))
             future = Future()
             self.futures[msg_id] = future
             return future
@@ -97,16 +86,7 @@ class Robot():
         raise NotImplementedError()
 
     def close(self):
-        self.running = False
-        with self.out_lock:
-            try:
-                self.socket_out.close()
-            except socket.error:
-                pass
-        try:
-            self.socket.close()
-        except socket.error:
-            pass
+        self.client.close_when_done()
 
     def __enter__(self):
         return self
