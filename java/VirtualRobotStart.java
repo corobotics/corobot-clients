93856d27f31d173600dd651eaba06a9ecb5a8451
pgalatic <pgalatic@gmail.com>
2016-07-26 13:27:19 -0400

    Add files via upload

--- java/VirtualRobotStart.java
+++ java/VirtualRobotStart.java
@@ -4,7 +4,19 @@
  */
 public class VirtualRobotStart{
 	public static void main(String args[]){
-		VirtualRobot r = new VirtualRobot();
-		r.startReading();	
+		if (args.length == 0) {
+			VirtualRobot r = new VirtualRobot();
+			r.startReading();
+		}else if (args.length == 1){
+			switch (args[0].toUpperCase()){
+				case "T":
+				case "TRUE":
+					VirtualRobot r = new VirtualRobot(true);
+					r.startReading();
+			}
+		}else{
+			System.err.print("Could not recognize arguments.");
+			System.out.println("USAGE: java VirtualRobotStart\nALT: java VirtualRobotStart T");
+		}
 	}
 }
