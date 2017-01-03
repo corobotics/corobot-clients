27a5a7cae0d389b7f0e4439623895bf9e77f2ba3
tappan-ajmera <tappan.ajmera@gmail.com>
2017-01-03 17:45:41 -0500

    Revert "Bug fix"
    This reverts commit 6b60d85eafc4fb2d6544eb2553a4b37a2e8f2ae5.

--- python3/corobot/map.py
+++ python3/corobot/map.py
@@ -15,19 +15,18 @@ class Map():
             f.next() # skip the first line
             for line in f:
                 parts = line.split(",")
-                name = parts[0].upper()
+                name = parts[0]
                 x, y = float(parts[3]), float(parts[4])
                 neighbors = parts[6:]
-                self.nodes[name] = self.Node(name, x, y, neighbors)
-
+                self.nodes[name] = Node(name, x, y, neighbors)
 
     def get_closest_node(self, pos):
         """Return the name of the closest node to the given position."""
         min_dist = float('+infinity')
         closest = None
         for name, x, y, _ in self.nodes.values():
-            dx = pos[0]- x
-            dy = pos[1] - y
+            dx = pos.x - x
+            dy = pos.y - y
             # Just use square distance since we're only comparing them.
             dist = dx * dx + dy * dy
             if dist < min_dist:
@@ -43,7 +42,6 @@ class Map():
         """Returns a Map.Node object for the given name."""
         return self.nodes[name.upper()]
 
-    def is_node(self,name):
+    def is_node(name):
         """Returns whether the given name represents a node in the map."""
         return name.upper() in self.nodes
-
